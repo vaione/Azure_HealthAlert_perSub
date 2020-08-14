a8296b210b213f6957dc7035d2b8d1d7fff10432
SSchiller91 <56626770+SSchiller91@users.noreply.github.com>
2020-08-14 16:35:01 +0200

    Update ActionGroup.ps1

--- ActionGroup.ps1
+++ ActionGroup.ps1
@@ -2,7 +2,7 @@
  
 $emailReceiverName = "emailreceiver"
 $emailAddress = "stephanschiller@gmx.net"
-$actionGroupName = "email-ag"
+$actionGroupName = "ServiceHealth"
 $actionGroupShortName = $actionGroupName
 $rgName = "PowershellTest"
 $tagName = "Environment"
@@ -19,22 +19,7 @@ $subscriptions=Get-AzureRMSubscription
 # Login-Az Account
 #Connect-AzAccount 
 
-## Create a new Action Group Email receiver
- 
-$emailReceiver = New-AzActionGroupReceiver -Name $emailReceiverName -EmailReceiver -EmailAddress $emailAddress
- 
-Write-Host ($writeEmptyLine + "# Action Group Receiver $emailReceiverName saved in memory" + $writeSeperator + $time)`
-#-foregroundcolor $foregroundColor1 $writeEmptyLine
 
-## Create a new Action Group
- 
-$tag = New-Object "System.Collections.Generic.Dictionary``2[System.String,System.String]"
-$tag.Add($tagName,$tagValue)
- 
-Set-AzActionGroup -Name $actionGroupName -ResourceGroup $rgName -ShortName $actionGroupShortName -Receiver $emailReceiver -Tag $tag
- 
-Write-Host ($writeEmptyLine + "# Action Group $actionGroupName created" + $writeSeperator + $time)`
--foregroundcolor $foregroundColor1 $writeEmptyLine
 $actiongroup = Get-AzActionGroup -Name $actionGroupName -ResourceGroup $rgName -WarningAction Ignore
 
 #For Loop through all Azure subscriptions
