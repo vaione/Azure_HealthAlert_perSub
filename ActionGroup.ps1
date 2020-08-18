919784876273210e59966cfbfb35daefa025f7a5
SSchiller91 <56626770+SSchiller91@users.noreply.github.com>
2020-08-18 09:50:42 +0200

    Update ActionGroup.ps1

--- ActionGroup.ps1
+++ ActionGroup.ps1
@@ -1,39 +1,28 @@
 ## Variables
- 
-$emailReceiverName = "emailreceiver"
-$emailAddress = "stephanschiller@gmx.net"
 $actionGroupName = "SHTest"
 $actionGroupShortName = $actionGroupName
 $rgName = "ServiceHealthTest"
 $location = "northeurope"
 $tagName = "Environment"
 $tagValue = "Demo"
-$time = Get-Date -UFormat "%A %m/%d/%Y %R"
-$foregroundColor1 = "Red"
-$writeEmptyLine = "`n"
-$writeSeperator = " - "
-$allResources = @()
 $subscriptions=Get-AzureRMSubscription
-
-# For backwards compatibility
-#Enable-AzureRmAlias
-# Login-Az Account
-#Connect-AzAccount 
 $email1 = New-Object  Microsoft.Azure.Commands.Insights.OutputClasses.PSEmailReceiver
-$email1.EmailAddress = "stephanschiller@gmx.net"
+$email1.EmailAddress = "maxmustermann@gmx.net"
 $email1.Name = "Stephan"
-$recievers = @($email1)
-
+$receivers = @($email1)
 $tag = New-Object "System.Collections.Generic.Dictionary``2[System.String,System.String]" 
 $tag.Add($tagName,$tagValue)
+
 
 #For Loop through all Azure subscriptions
 ForEach ($vsub in $subscriptions){
 Select-AzSubscription $vsub.SubscriptionID
 
 Write-Host “Working on “ $vsub
+#Create resource group
 New-AzResourceGroup -Name $rgName -Location $location
-Set-AzureRmActionGroup -Name $actionGroupName -ResourceGroup $rgName -ShortName $actionGroupShortName -Receiver $recievers -Tag $tag
+#Create action group
+Set-AzureRmActionGroup -Name $actionGroupName -ResourceGroup $rgName -ShortName $actionGroupShortName -Receiver $receivers -Tag $tag
 $actiongroup = Get-AzureRmActionGroup -Name $actionGroupName -ResourceGroup $rgName 
 $params = @{
     activityLogAlertName = "TamOps Azure Service Notification" + $vsub.Name
