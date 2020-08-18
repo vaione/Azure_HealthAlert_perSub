f0d9f38eebd0bebef10a940ed2080976bb8a81c7
SSchiller91 <56626770+SSchiller91@users.noreply.github.com>
2020-08-18 09:08:14 +0200

    Update ActionGroup.ps1

--- ActionGroup.ps1
+++ ActionGroup.ps1
@@ -15,7 +15,7 @@ $allResources = @()
 $subscriptions=Get-AzureRMSubscription
 
 # For backwards compatibility
-#Enable-AzureRmAlias
+Enable-AzureRmAlias
 # Login-Az Account
 #Connect-AzAccount 
 
@@ -27,7 +27,7 @@ ForEach ($vsub in $subscriptions){
 Select-AzSubscription $vsub.SubscriptionID
 
 Write-Host “Working on “ $vsub
-Set-AzureRmActionGroup -Name $actionGroupName -ResourceGroup $rgName -ShortName $actionGroupShortName -Receiver $emailReceiver -Tag $tag
+Set-AzureRmActionGroup -Name $actionGroupName -ResourceGroup $rgName -ShortName $actionGroupShortName -Receiver $emailReceiverName -Tag $tag
 $actiongroup = Get-AzureRmActionGroup -Name $actionGroupName -ResourceGroup $rgName 
 $params = @{
     activityLogAlertName = "TamOps Azure Service Notification" + $vsub.Name
